549d1708dbca151f29b2b0f896a7c8536e4c6248
Samridh Tuladhar <t.samridh.86@gmail.com>
2016-06-08 22:06:56 +0545

    display advanced text also

--- search.php
+++ search.php
@@ -17,7 +17,18 @@
     		while($row = $result->fetch_assoc())
     		  showArticle($row['heading'],$row['firstName']." ".$row['lastName'],$row['textLocation'] , $row['inputType'], $row['category'],$row['imgLoc'], $row['timeOfUpload']);
     		$connect->close();
-    	}	
+    	}
+    if (!empty($_GET['category']))
+        {
+            echo "<h3 class = 'heading' > News for ".$_GET['category']." : </h3>";
+            require 'config/dbaccess.php';
+            require 'config/functionBundle.php';
+            $que = "select * from article natural join users where category = '".$_GET['category']."' order by timeOfUpload desc; ";
+            $result = $connect->query($que);
+            while($row = $result->fetch_assoc())
+              showArticle($row['heading'],$row['firstName']." ".$row['lastName'],$row['textLocation'] , $row['inputType'], $row['category'],$row['imgLoc'], $row['timeOfUpload']);
+            $connect->close();
+        }   	
  ?>
 </div>	
 </div>
